e22e77decaed63043b175d615d98435b83922ebd
josemourinho333 <yoo.phil92@gmail.com>
2022-06-29 18:24:09 -0700

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,7 +1,4 @@
-// const express = require('express');
-// const morgan = require('morgan');
 const pg = require('pg');
-// const port = process.env.PORT || 8000;
 
 const config = {
   user: 'labber',
@@ -12,31 +9,18 @@ const config = {
 
 const pool = new pg.Pool(config);
 
-// pool.connect(() => {
-//   console.log('connected to database server');
-// });
-
 const cohortName = process.argv[2];
 const limitBy = process.argv[3];
 
-// pool.query(`
-//     SELECT students.id as student_id, students.name AS name, cohorts.name AS cohort
-//     FROM students
-//     JOIN cohorts ON cohorts.id = students.cohort_id
-//     LIMIT 5;
-//   `)
-//   .then(response => {
-//     console.log(response.rows);
-//   })
-//   .catch(error => console.error('query error', error.stack));
+const queryString = `
+  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-    SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = students.cohort_id
-    WHERE cohorts.name LIKE '${cohortName}%'
-    LIMIT ${limitBy};
-  `)
+pool.query(queryString, [`%${cohortName}%`, limitBy])
   .then( response => {
     response.rows.forEach( user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,4 @@
-// const express = require('express');
-// const morgan = require('morgan');
 const pg = require('pg');
-// const port = process.env.PORT || 8000;
 
 const config = {
   user: 'labber',
@@ -15,14 +12,16 @@ const pool = new pg.Pool(config);
 const cohortName = process.argv[2];
 const limitBy = process.argv[3];
 
-pool.query(`
+const queryString = `
     SELECT DISTINCT(teachers.name) AS name, cohorts.name AS cohort
     FROM assistance_requests
     JOIN teachers ON teachers.id = assistance_requests.teacher_id
     JOIN students ON students.id = assistance_requests.student_id
     JOIN cohorts ON cohorts.id = students.cohort_id
-    WHERE cohorts.name = '${cohortName}';
-  `)
+    WHERE cohorts.name = $1;
+  `;
+
+pool.query(queryString, [cohortName])
   .then( response => {
     response.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.name}`);
